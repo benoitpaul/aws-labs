00b6b9776a6f3adba0914121d790c29fe4cb1612
Benoit Paul <benoit.paul@gmail.com>
2022-11-26 15:30:49 -0500

    feat: default-vpc-ec2

---
+++ default-vpc-ec2/README.md
@@ -0,0 +1,12 @@
+# Simple EC2 instance in default VPC
+
+This demo showcases how to create and launch an EC2 instance in the default VPC, with a security group
+
+The security groups allows inbound traffic from:
+
+- SSH (TCP port 22)
+- HTTP (TCP port 80)
+
+![Architecture](https://github.com/benoitpaul/aws-labs/raw/main/default-vpc-ec2/Architecture.png)
+
+Blog post available at: [https://www.benoitpaul.com/aws/default-vpc-ec2/](https://www.benoitpaul.com/aws/default-vpc-ec2/)
---
+++ default-vpc-ec2/lib/default-vpc-ec2-stack.ts
@@ -0,0 +1,56 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { readFileSync } from "fs";
+
+export class DefaultVpcEc2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this, "VPC", {
+      isDefault: true,
+    });
+
+    // Security group for the EC2 instance
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc,
+      description: "Allow SSH (TCP port 22) and HTTP (TCP port 80) in",
+      allowAllOutbound: true,
+    });
+
+    // Allow SSH access on port tcp/22
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "Allow SSH Access"
+    );
+
+    // Allow HTTP access on port tcp/80
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      "Allow HTTP Access"
+    );
+
+    // latest Amazon Linux 2 Image with CPU Type X86_64
+    const ami = ec2.MachineImage.latestAmazonLinux({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: ec2.AmazonLinuxCpuType.X86_64,
+    });
+
+    // Create the EC2 instance using the Security Group and AMI defined.
+    const ec2Instance = new ec2.Instance(this, "Instance", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ami,
+      securityGroup: securityGroup,
+    });
+
+    // 👇 load contents of script
+    const userData = readFileSync("./data/user-data.sh", "utf8");
+    ec2Instance.addUserData(userData);
+  }
+}
